2b8750226899b3c628961b59bcdf55869da5be7a
Rodion Shpak <rodion.it22@gmail.com>
2022-08-11 19:58:16 +0300

    com

---
+++ src/js/03-feedback.js
@@ -0,0 +1,40 @@
+
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
+
+let formData = {};
+
+function onFormData(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+    e.preventDefault();
+
+    const email = document.querySelector('.feedback-form input');
+    const message = document.querySelector('.feedback-form textarea');
+    if (message.value === '' || email.value === '') {
+        alert(`Все поля должны быть заполнены`);
+    } else {
+        e.currentTarget.reset();
+        localStorage.removeItem(STORAGE_KEY);
+        console.log(formData);
+    }
+}
+
+(function updateDataFromLocalStorage() {
+    let data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if (data) {
+        Object.entries(data).forEach(([key, value]) => {
+            formData[key] = value;
+            form.elements[key].value = value;
+        });
+    }
+})();
